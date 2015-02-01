b9164e876856e817204850aa0a3b7668e5993d04
Quentin <deslandesquentin17@gmail.com>
2015-02-01 14:13:42 +0100

    plusOuMoins.c - Add menu displaying && main loop

--- exercises/corrections/plusOuMoins.c
+++ exercises/corrections/plusOuMoins.c
@@ -7,13 +7,22 @@
 #define LIVES 5
 
 int randint(int min, int max);
+void menu();
 
 int main(void) {
 	// On définit le point de départ de rand()
 	srand(time(NULL));
 
-	for (int i = LIVES; i--; ) {
-		printf("%d\n", randint(MIN, MAX));
+	int menu_choice = -1;
+	while (menu_choice) {
+		if (menu_choice == -1) {
+			menu();
+			scanf("%d", &menu_choice);
+		} else if (menu_choice == 1) {
+
+		} else if (menu_choice == 2) {
+
+		}
 	}
 
 	return 0;
@@ -22,4 +31,13 @@ int main(void) {
 /* randint : return a randomly generated number between min and max */
 int randint(int min, int max) {
 	return rand() % (max - min) + min;
+}
+
+/* menu : display the game menu */
+void menu() {
+	printf("Welcome to + or - !\n\n");
+	printf("Please, take a seat and enjoy !\n\n");
+	printf("1 - Play alone\n");
+	printf("2 - Play with a friend\n");
+	printf("0 - Quit\n");
 }
