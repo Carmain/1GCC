971444f17f79f627b8bd986db6fa5f97e07237b0
Quentin <deslandesquentin17@gmail.com>
2015-02-01 14:06:12 +0100

    plusOuMoins.c - Add randint(int min, int max) function

--- exercises/corrections/plusOuMoins.c
+++ exercises/corrections/plusOuMoins.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MIN 0
+#define MAX 100
+#define LIVES 5
+
+int randint(int min, int max);
 
 int main(void) {
+	// On définit le point de départ de rand()
+	srand(time(NULL));
+
+	for (int i = LIVES; i--; ) {
+		printf("%d\n", randint(MIN, MAX));
+	}
 
 	return 0;
+}
+
+/* randint : return a randomly generated number between min and max */
+int randint(int min, int max) {
+	return rand() % (max - min) + min;
 }
