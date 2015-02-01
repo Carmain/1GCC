2c1f562ada24cb2f597658935c97b804b4ab1360
Quentin <deslandesquentin17@gmail.com>
2015-02-01 13:28:49 +0100

    Add fizzbuzzImproved - Working

---
+++ exercises/corrections/fizzbuzzImproved.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LIMIT 100
+
+const char *words[] = {"%d ", "fizz ", "buzz ", "fizzbuzz "};
+
+int main(void) {
+	printf("FIZZBUZZ exercise, from 1 to %d :\n\n", LIMIT);
+
+	for (int i = 1; i < LIMIT; ++i) {
+		printf(words[!(i % 3) + 2 * !(i % 5)], i);
+	}
+
+	return 0;
+}
